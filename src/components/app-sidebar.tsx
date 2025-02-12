29851e15e909e31915444ed358012179a6e3a427
ZackHan-dot <15373303031@163.com>
2025-02-12 08:42:05 +0800

    fix: 修复无限获取用户信息bug

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -57,7 +57,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
     useEffect(() => {
         handleGetUserProfile();
-    }, [userInfo]);
+    }, []);
 
     return (
         <Sidebar variant="inset" {...props}>
